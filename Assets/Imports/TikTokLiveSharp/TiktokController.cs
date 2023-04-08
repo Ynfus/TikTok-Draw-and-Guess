6b17cc1915772749ea48e95f1d49fc37a3a0b467
Ynfus <95777380+Ynfus@users.noreply.github.com>
2023-04-08 10:23:12 +0200

    Update

--- Assets/Imports/TikTokLiveSharp/TiktokController.cs
+++ Assets/Imports/TikTokLiveSharp/TiktokController.cs
@@ -9,33 +9,36 @@ using UnityEngine.Networking;
 using TikTokLiveSharp.Client;
 using TikTokLiveSharp.Models;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 
 public class TiktokController : MonoBehaviour
 {
-
     public string id;
-
-    [SerializeField] TextMeshProUGUI textMeshProUGUI;
-
+    public TextMeshProUGUI textMeshProUGUI;
+    [SerializeField] private Scrollbar scrollbar;
     private TikTokLiveClient _client;
+    private Queue<string> _comments = new Queue<string>();
 
-    private Queue<string> _profilePictures;
+    private string _selectedWord = "";
+
     void Start()
     {
         _client = new TikTokLiveClient(id);
-        _profilePictures = new Queue<string>();
 
         _client.OnCommentRecieved += Client_OnCommentRecieved;
         try
         {
-
             _client.Start();
         }
         catch (System.Exception e)
         {
             Debug.LogException(e);
         }
+        PlayerPrefs.SetString("SelectedWord", "hej");
+        _selectedWord = PlayerPrefs.GetString("SelectedWord", "");
     }
+
     void OnDestroy()
     {
         _client.Stop();
@@ -44,45 +47,190 @@ public class TiktokController : MonoBehaviour
 
     void Client_OnCommentRecieved(object sender, WebcastChatMessage e)
     {
-        Debug.Log(e.Comment);
-        lock (_profilePictures)
+        Debug.Log(e.User.Nickname + ": " + e.Comment);
+
+        lock (_comments)
         {
-            var url = e.User.profilePicture.Urls.FirstOrDefault(x => x.Contains(".jpeg"));
-            if (url != null)
+            _comments.Enqueue(e.User.Nickname + ": " + e.Comment);
+        }
+
+        if (!string.IsNullOrEmpty(_selectedWord))
+        {
+            string selectedWord = _selectedWord.ToLower();
+            string comment = e.Comment.ToLower();
+            if (selectedWord == comment)
             {
-                _profilePictures.Enqueue(url);
-                _profilePictures.Enqueue(e.Comment);
+                string nickname = e.User.Nickname;
+                Debug.Log(nickname + " znalaz³(a) s³owo: " + selectedWord);
+
+                PlayerPrefs.SetString("SelectedWord", "");
+
+                _selectedWord = "";
             }
         }
     }
 
     void Update()
     {
-        lock (_profilePictures)
+        lock (_comments)
         {
-            if (_profilePictures.Count > 0)
+            while (_comments.Count > 0)
             {
-                var url = _profilePictures.Dequeue();
-                var comment = _profilePictures.Dequeue();
-                StartCoroutine(AddNewCube(url, comment));
+                var comment = _comments.Dequeue();
+                textMeshProUGUI.text += "\n" + comment;
+                ScrollToBottom();
             }
         }
-
     }
-    IEnumerator AddNewCube(string url, string comment)
+    void ScrollToBottom()
     {
-        var request = UnityWebRequestTexture.GetTexture(url);
-        yield return request.SendWebRequest();
-        var tex = DownloadHandlerTexture.GetContent(request);
-
-        var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        obj.transform.position = Random.insideUnitSphere * 5;
-        obj.GetComponent<MeshRenderer>().material.mainTexture = tex;
-
-        //var textObj = new GameObject("Text");
-        //textObj.transform.parent = obj.transform;
-        //textObj.transform.localPosition = new Vector3(0, 0.5f, 0);
-        //var textMesh = textObj.AddComponent<TextMesh>();
-        textMeshProUGUI.text += "\n"+ comment;
+        scrollbar.value = 0f;
     }
+
+
+
+
+
+
+
+
+
+
+    //public string id;
+    //[SerializeField] TextMeshProUGUI textMeshProUGUI;
+    //private TikTokLiveClient _client;
+    //private Queue<string> _comments;
+
+    //void Start()
+    //{
+    //    _client = new TikTokLiveClient(id);
+    //    _comments = new Queue<string>();
+
+    //    _client.OnCommentRecieved += Client_OnCommentRecieved;
+    //    try
+    //    {
+    //        _client.Start();
+    //    }
+    //    catch (System.Exception e)
+    //    {
+    //        Debug.LogException(e);
+    //    }
+    //    PlayerPrefs.SetString("SelectedWord", "Czeœæ");
+    //}
+
+    //void OnDestroy()
+    //{
+    //    _client.Stop();
+    //    _client.OnCommentRecieved -= Client_OnCommentRecieved;
+    //}
+
+    //void Client_OnCommentRecieved(object sender, WebcastChatMessage e)
+    //{
+    //    Debug.Log(e.User.Nickname + ": " + e.Comment);
+    //    lock (_comments)
+    //    {
+    //        _comments.Enqueue(e.User.Nickname + ": " + e.Comment);
+    //    }
+    //    if (!string.IsNullOrEmpty(PlayerPrefs.GetString("SelectedWord")))
+    //    {
+    //        string selectedWord = PlayerPrefs.GetString("SelectedWord").ToLower();
+    //        string comment = e.Comment.ToLower();
+    //        if (selectedWord == comment)
+    //        {
+    //            string nickname = e.User.Nickname;
+    //            Debug.Log(nickname + " znalaz³(a) s³owo: " + selectedWord);
+    //            PlayerPrefs.SetString("SelectedWord", "");
+    //        }
+    //    }
+    //}
+
+    //void Update()
+    //{
+    //    Debug.Log($"Iloœæ komentarzy w kolejce: {_comments.Count}");
+    //    lock (_comments)
+    //    {
+    //        while (_comments.Count > 0)
+    //        {
+    //            var comment = _comments.Dequeue();
+    //            textMeshProUGUI.text += "\n" + comment;
+    //        }
+    //    }
+    //}
+
+
+
+
+
+    //public string id;
+
+    //[SerializeField] TextMeshProUGUI textMeshProUGUI;
+
+    //private TikTokLiveClient _client;
+
+    //private Queue<string> _profilePictures;
+    //void Start()
+    //{
+    //    _client = new TikTokLiveClient(id);
+    //    _profilePictures = new Queue<string>();
+
+    //    _client.OnCommentRecieved += Client_OnCommentRecieved;
+    //    try
+    //    {
+
+    //        _client.Start();
+    //    }
+    //    catch (System.Exception e)
+    //    {
+    //        Debug.LogException(e);
+    //    }
+    //}
+    //void OnDestroy()
+    //{
+    //    _client.Stop();
+    //    _client.OnCommentRecieved -= Client_OnCommentRecieved;
+    //}
+
+    //void Client_OnCommentRecieved(object sender, WebcastChatMessage e)
+    //{
+    //    Debug.Log(e.Comment);
+    //    lock (_profilePictures)
+    //    {
+    //        var url = e.User.profilePicture.Urls.FirstOrDefault(x => x.Contains(".jpeg"));
+    //        if (url != null)
+    //        {
+    //            _profilePictures.Enqueue(url);
+    //            _profilePictures.Enqueue(e.Comment);
+    //        }
+    //    }
+    //}
+
+    //void Update()
+    //{
+    //    lock (_profilePictures)
+    //    {
+    //        if (_profilePictures.Count > 0)
+    //        {
+    //            var url = _profilePictures.Dequeue();
+    //            var comment = _profilePictures.Dequeue();
+    //            StartCoroutine(AddNewCube(url, comment));
+    //        }
+    //    }
+
+    //}
+    //IEnumerator AddNewCube(string url, string comment)
+    //{
+    //    var request = UnityWebRequestTexture.GetTexture(url);
+    //    yield return request.SendWebRequest();
+    //    var tex = DownloadHandlerTexture.GetContent(request);
+
+    //    var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //    obj.transform.position = Random.insideUnitSphere * 5;
+    //    obj.GetComponent<MeshRenderer>().material.mainTexture = tex;
+
+    //    //var textObj = new GameObject("Text");
+    //    //textObj.transform.parent = obj.transform;
+    //    //textObj.transform.localPosition = new Vector3(0, 0.5f, 0);
+    //    //var textMesh = textObj.AddComponent<TextMesh>();
+    //    textMeshProUGUI.text += "\n" + comment;
+    //}
 }
