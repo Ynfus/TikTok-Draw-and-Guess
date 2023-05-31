b32c26aa5164857a9e55b6c77abce47457e02bef
Ynfus <95777380+Ynfus@users.noreply.github.com>
2023-05-31 14:00:43 +0200

    Try WebGl | Update

--- Assets/DrawMesh.cs
+++ Assets/DrawMesh.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security;
-using UnityEditor;
+//using UnityEditor;
 using UnityEngine;
 
 public class DrawMesh : MonoBehaviour
